8ef714e22ba1f06db514470757b51f88ee198aa8
szymon niedorezo <szymonniedorezo@gmail.com>
2024-09-26 11:29:17 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update", as: :update
-  delete "tasks/:id", to: "tasks#destroy"
-  get "tasks/:id", to: "tasks#details", as: :task
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
